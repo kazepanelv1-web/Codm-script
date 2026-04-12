5cacc72d087b25f6b868359c6490a5a1264767ec
kazepanelv1-web <kazepanelv1@gmail.com>
2026-04-12 21:18:01 +0800

    Update panel.py

--- panel.py
+++ panel.py
@@ -14,7 +14,7 @@ CORS(app)
 # ======================
 # CONSTANTS
 # ======================
-TOKEN_EXPIRY = 60      # seconds for token expiry
+TOKEN_EXPIRY = 20      # seconds for token expiry
 COOLDOWN = 120           # anti-spam cooldown
 KEY_LIMIT = 120         # seconds before same IP can generate another key
 DATA_FILE = "database.json"
